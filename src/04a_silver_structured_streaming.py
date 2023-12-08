ba3d26f8ac4b4ec2b18d9d4bcd9493edd3395329
aadheeAntham <aadheeAntham>
2023-12-08 22:11:19 +0000

    Update for Oil Dimensions

--- src/04a_silver_structured_streaming.py
+++ src/04a_silver_structured_streaming.py
@@ -298,4 +298,51 @@ read_bronze_transaction = spark.readStream.format("delta")\
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC ## silver_dim_oil
+# MAGIC
+# MAGIC
+
+# COMMAND ----------
+
+# Function to upsert microBatchOutputDF into Delta table using merge
+def upsertToDeltaTransactions(microBatchOutputDF, batchId):
+  # Set the dataframe to view name
+  microBatchOutputDF.createOrReplaceTempView("dim_oil_updates")
+
+  # Use the view name to apply MERGE
+  # NOTE: You have to use the SparkSession that has been used to define the `updates` dataframe
+
+  # In Databricks Runtime 10.5 and below, you must use the following:
+  # microBatchOutputDF._jdf.sparkSession().sql("""
+  microBatchOutputDF.sparkSession.sql("""
+    MERGE INTO silver_dim_oil t
+    USING dim_oil_updates s
+    ON t.date = s.date
+    WHEN MATCHED THEN UPDATE SET 
+        t.oil_price = s.oil_price
+        WHEN NOT MATCHED THEN INSERT 
+        (`date`,
+        oil_price)
+        VALUES 
+        (s.date,
+        s.oil_price)
+
+  """)
+
+# create a read stream
+read_bronze_transaction = spark.readStream.format("delta")\
+  .table("bronze_oil")
+
+# Write the output of a streaming aggregation query into Delta table
+(read_bronze_transaction.writeStream
+  .format("delta")
+  .foreachBatch(upsertToDeltaTransactions)
+  .outputMode("update")
+  .option("checkpointLocation","/mnt/g5/silver/checkpointDimOil")
+  .start()
+)
+
+# COMMAND ----------
+
 
